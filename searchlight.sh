25f20f1ddd68a995f78ef4b30a159bb8af75faba
kaihwang <kai.hwang@gmail.com>
2023-10-09 14:39:00 -0500

    commit.

--- searchlight.sh
+++ searchlight.sh
@@ -1,9 +1,9 @@
 # run searchlight on thalahi dataset.
 #$ -N thalhi_sl
 #$ -q SEASHORE
-#$ -pe smp 4
-#$ -t 1-50
-#$ -tc 24
+#$ -pe smp 24
+#$ -t 1-73
+#$ -tc 6
 #$ -ckpt user
 #$ -o /Users/kahwang/sge_logs/
 #$ -e /Users/kahwang/sge_logs/
@@ -14,9 +14,10 @@ source activate py3.8
 /bin/echo In directory: `pwd`
 /bin/echo Starting on: `date`
 
-subjects=(10001  10014  10027  10038  10068 10002  10017  10028  10040  10071 10003  10018  10029  10041  10073 10004  10019  10030  10042  10074 10005  10020  10031  10043  10076 10007  10021  10032  10054  10077 10008  10022  10033  10059  10080 10010  10023  10034  10060 10024  10035  10063 10012  10025  10036  10064 10013  10026  10037  10066)
+subjects=(10001  10006  10011  10017  10022  10027  10032  10037  10042  10057  10062  10068  10074  10169  10175 10002  10007  10012  10018  10023  10028  10033  10038  10043  10058  10063  10069  10076  10170  10176 10003  10008  10013  10019  10024  10029  10034  10039  10044  10059  10064  10071  10077  10172  10179 10004  10009  10014  10020  10025  10030  10035  10040  10054  10060  10065  10072  10080  10173 10005  10010  10016  10021  10026  10031  10036  10041  10055  10061  10066  10073  10162  10174)
 echo subjects: ${subjects[@]}
 echo total_subjects=${#subjects[@]}
 subject="${subjects[$SGE_TASK_ID-1]}"
 echo "Starting decoding on $subject"
 echo $subject | python /Users/kahwang/bin/ThalHi_MRI/test_searchlight.py
+
--- test_searchlight.py
+++ test_searchlight.py
@@ -22,10 +22,13 @@ import datetime
 #plt.ion()
 
 subject = input()
-#subject='10011'
-func_path = "/Shared/lss_kahwang_hpc/data/ThalHi/3dDeconvolve/sub-%s/" %subject
+#subject='10176'
+func_path = "/Shared/lss_kahwang_hpc/data/ThalHi/3dDeconvolve_fdpt4/sub-%s/" %subject
 version_info = pd.read_csv("/Shared/lss_kahwang_hpc/data/ThalHi/Version_Info.csv")
-version = version_info.loc[version_info['sub'] == int(subject), 'version'].values[0]
+try:
+    version = version_info.loc[version_info['sub'] == int(subject), 'version'].values[0]
+except:
+    version = 'DCFS'
 
 ####  classification with SVM
 ### compile all the lss cue outputs
@@ -50,14 +53,15 @@ for i, cue in enumerate(cues):
 all_cues_img = nib.funcs.concat_images(comebined_cues, axis=3)
 
 num_of_betas = all_cues_img.shape[3]
-run_record = pd.read_csv(func_path + "compiled_conditions.csv")
 
-if num_of_betas == len(run_record):
+try:
+    run_record = pd.read_csv(func_path + "compiled_conditions.csv")
+    #if num_of_betas == len(run_record):
     runs = []
     for cue in cues:
         runs = runs + run_record.loc[run_record['Cue'] == cue, 'Run'].values.tolist()
     runs = np.array(runs)
-else:
+except:
     runs = []
     for i, cue in enumerate(cues):
         runs = runs + np.repeat(i+1, int(cue_objects[i].shape[3]/2)).tolist()
@@ -118,29 +122,51 @@ print(now)
 mean_fmri = nilearn.image.mean_img(all_cues_img)
 pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
 
-# searchlight_context = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=4, verbose=0, cv=LeaveOneGroupOut())
-# searchlight_context.fit(all_cues_img, context_Y, runs)
-# context_score_img = new_img_like(mean_fmri, searchlight_context.scores_)
-# context_score_img.to_filename(func_path + "%s_context_svc_score.nii.gz" %subject)
+try:
+    searchlight_context = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+    searchlight_context.fit(all_cues_img, context_Y, runs)
+    context_score_img = new_img_like(mean_fmri, searchlight_context.scores_)
+    context_score_img.to_filename(func_path + "%s_context_svc_score.nii.gz" %subject)
+    now = datetime.datetime.now()
+    print(now)
+except:
+    print("context broke")
+    pass
 
-# searchlight_color = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=4, verbose=0, cv=LeaveOneGroupOut())
-# searchlight_color.fit(all_cues_img, color_Y, runs)
-# color_score_img = new_img_like(mean_fmri, searchlight_color.scores_)
-# color_score_img.to_filename(func_path + "%s_color_svc_score.nii.gz" %subject)
+try:
+    pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
+    searchlight_color = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+    searchlight_color.fit(all_cues_img, color_Y, runs)
+    color_score_img = new_img_like(mean_fmri, searchlight_color.scores_)
+    color_score_img.to_filename(func_path + "%s_color_svc_score.nii.gz" %subject)
+    now = datetime.datetime.now()
+    print(now)
+except:
+    print("color broke")
+    pass
 
-searchlight_shape = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
-searchlight_shape.fit(all_cues_img, shape_Y, runs)
-shape_score_img = new_img_like(mean_fmri, searchlight_shape.scores_)
-shape_score_img.to_filename(func_path + "%s_shape_svc_score.nii.gz" %subject)
+try:
+    pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
+    searchlight_shape = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+    searchlight_shape.fit(all_cues_img, shape_Y, runs)
+    shape_score_img = new_img_like(mean_fmri, searchlight_shape.scores_)
+    shape_score_img.to_filename(func_path + "%s_shape_svc_score.nii.gz" %subject)
+    now = datetime.datetime.now()
+    print(now)
+except:
+    print("shape broke")
+    pass
 
-searchlight_task = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
-searchlight_task.fit(all_cues_img, task_Y, runs)
-task_score_img = new_img_like(mean_fmri, searchlight_task.scores_)
-task_score_img.to_filename(func_path + "%s_task_svc_score.nii.gz" %subject)
-
-
-now = datetime.datetime.now()
-print(now)
-
+try:
+    pipeline = make_pipeline(StandardScaler(), LogisticRegression('l1', solver='liblinear')) 
+    searchlight_task = nilearn.decoding.SearchLight(mask, process_mask_img=mask, estimator = pipeline, radius=6, n_jobs=24, verbose=0, cv=LeaveOneGroupOut())
+    searchlight_task.fit(all_cues_img, task_Y, runs)
+    task_score_img = new_img_like(mean_fmri, searchlight_task.scores_)
+    task_score_img.to_filename(func_path + "%s_task_svc_score.nii.gz" %subject)
+    now = datetime.datetime.now()
+    print(now)
+except:
+    print("task broke")
+    pass
 
 ### end
